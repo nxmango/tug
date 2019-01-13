46f2c2b0715e9e86f9263114355e72a8047bc3da
nxmango <46639448+nxmango@users.noreply.github.com>
2019-01-13 13:09:15 -0500

    Update usb_install_pc.py

--- usb_install_pc.py
+++ usb_install_pc.py
@@ -1,34 +1,9 @@
-# This script depends on PyUSB. You can get it with pip install pyusb.
-# You will also need libusb installed
-#
-# Windows Instructions:
-# 1. Download Zadig from https://zadig.akeo.ie/.
-# 2. With your Switch plugged in and on the Tinfoil USB install menu,
-#    choose "List All Devices" under the options menu in Zadig, and select libnx USB comms.
-# 3. Choose libusbK from the driver list and click the "Replace Driver" button.
-# 4. Go to Tinfoil > Title Management > USB Install NSP
-# 5. Run this script
-#      python usb_install_pc.py <path/to/nsp_folder>
-
-# macOS Instructions:
-# 1. Install Homebrew https://brew.sh
-# 2. Install Python 3
-#      sudo mkdir /usr/local/Frameworks
-#      sudo chown $(whoami) /usr/local/Frameworks
-#      brew install python
-# 3. Install PyUSB
-#      pip3 install pyusb
-# 4. Install libusb
-#      brew install libusb
-# 5. Plug in your Switch and go to Tinfoil > Title Management > USB Install NSP
-# 6. Run this script
-#      python3 usb_install_pc.py <path/to/nsp_folder>
+# Slightly modified by nxmango from https://github.com/Adubbz/Tinfoil/blob/master/tools/usb_install_pc.py
 
 import struct
 
 CMD_ID_EXIT = 0
 CMD_ID_FILE_RANGE = 1
-
 CMD_TYPE_RESPONSE = 1
 
 def send_response_header(out_ep, cmd_id, data_size):
@@ -45,22 +20,18 @@ def file_range_cmd(nsp_dir, in_ep, out_ep, data_size):
     range_size = struct.unpack('<Q', file_range_header[:8])[0]
     range_offset = struct.unpack('<Q', file_range_header[8:16])[0]
     nsp_name_len = struct.unpack('<Q', file_range_header[16:24])[0]
-    # in_ep.read(0x8) # Reserved
     nsp_name = bytes(in_ep.read(nsp_name_len)).decode('utf-8')
 
     send_response_header(out_ep, CMD_ID_FILE_RANGE, range_size)
 
     with open(nsp_name, 'rb') as f:
         f.seek(range_offset)
-
         curr_off = 0x0
         end_off = range_size
         read_size = 0x800000
-
         while curr_off < end_off:
             if curr_off + read_size >= end_off:
                 read_size = end_off - curr_off
-
             buf = f.read(read_size)
             out_ep.write(data=buf, timeout=0)
             curr_off += read_size
@@ -72,7 +43,6 @@ def poll_commands(nsp_dir, in_ep, out_ep):
 
         if magic != b'TUC0':  # Tinfoil USB Command 0
             continue
-
         cmd_type = struct.unpack('<B', cmd_header[4:5])[0]
         cmd_id = struct.unpack('<I', cmd_header[8:12])[0]
         data_size = struct.unpack('<Q', cmd_header[12:20])[0]
@@ -81,7 +51,6 @@ def poll_commands(nsp_dir, in_ep, out_ep):
             break
         elif cmd_id == CMD_ID_FILE_RANGE:
             file_range_cmd(nsp_dir, in_ep, out_ep, data_size)
-
 
 def send_nsp_list(nsp_dir, out_ep):
     nsp_path_list = list()
