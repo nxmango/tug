b903134a3cac14093f948b0bff6ca32aa7250d22
nxmango <46639448+nxmango@users.noreply.github.com>
2019-01-13 14:55:26 -0500

    Update tug.py
    Better optimization & cleaner code. 
    Fixed a major problem: Crashing when trying to send header multiple times without closing TUG.
    Nintendo Switch will now be found on startup making it possible to send the same header multiple times. TUG will prompt an error if it can't find the device & if the connection is lost when trying to send header.

--- tug.py
+++ tug.py
@@ -31,15 +31,19 @@ from pathlib import Path
 from tkinter import messagebox
 from usb_install_pc import send_nsp_list, poll_commands
 import tkinter as tk
+
+"""
+Exceptions for imports (PyUSB & PyQt5)
+from fourminute
+"""
 root = tk.Tk()
 root.withdraw()
 try:
     import usb.core
     import usb.util
 except ImportError:
-    messagebox.showinfo("Error","PyUSB not found. Please install with 'pip3 install pyusb'\nIf you are on mac, also install LibUSB with 'brew install libusb'.")
+    messagebox.showinfo("Error","PyUSB not found. Please install with 'pip3 install pyusb'\nIf you are on macOS, also install LibUSB with 'brew install libusb'.")
     exit()
-
 try:
     from PyQt5 import QtWidgets, QtGui
     from PyQt5.QtWidgets import QFileDialog, QListWidget, QMessageBox
@@ -51,15 +55,31 @@ class TUG(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
-        self.init_ui()
-
-    def init_ui(self):
         self.b_folder = QtWidgets.QPushButton("Select folder")
         self.b_header = QtWidgets.QPushButton("Send header")
         self.b_header.setEnabled(False)
         self.l_folder = QtWidgets.QLabel("No folder selected")
         self.list_header = QtWidgets.QListWidget()
         self.folder = ""
+
+        # Find Nintendo Switch
+        self.dev = usb.core.find(idVendor=0x057E, idProduct=0x3000)
+        if self.dev is None:
+            QMessageBox.warning(self, "Warning", "Nintendo Switch can't be found, try again.")
+            exit()
+        self.dev.reset()
+        self.dev.set_configuration()
+        cfg = self.dev.get_active_configuration()
+        is_out_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_OUT
+        is_in_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_IN
+        self.out_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_out_ep)
+        self.in_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_in_ep)
+        assert self.out_ep is not None
+        assert self.in_ep is not None
+
+        self.init_ui()
+
+    def init_ui(self):
 
         v_box = QtWidgets.QVBoxLayout()
         v_box.addWidget(self.b_folder)
@@ -97,34 +117,15 @@ class TUG(QtWidgets.QWidget):
             pass
         if len(self.folder) > 0:
             self.b_header.setEnabled(True)
+            self.folder = Path(self.folder)
 
     def send_header(self):
         try:
-            nsp_dir = Path(self.folder)
+            send_nsp_list(self.folder, self.out_ep)
+            poll_commands(self.folder, self.in_ep, self.out_ep)
         except:
-            pass
-
-        dev = usb.core.find(idVendor=0x057E, idProduct=0x3000)
-        if dev is None:
-            QMessageBox.warning(self, "Warning", "Couldn't find Nintendo Switch")
-
-        try:
-            dev.reset()
-            dev.set_configuration()
-            cfg = dev.get_active_configuration()
-
-            is_out_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_OUT
-            is_in_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_IN
-            out_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_out_ep)
-            in_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_in_ep)
-
-            assert out_ep is not None
-            assert in_ep is not None
-
-            send_nsp_list(nsp_dir, out_ep)
-            poll_commands(nsp_dir, in_ep, out_ep)
-        except:
-            pass
+            QMessageBox.critical(self, "Critical", "Connection lost to Nintendo Switch, exiting.")
+            exit()
 
 app = QtWidgets.QApplication(sys.argv)
 ex = TUG()
