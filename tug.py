98db693b3cf94e61bcbef867af8db14560195951
fourminute <38321299+fourminute@users.noreply.github.com>
2019-01-14 13:21:30 -0800

    Progress bar implementation and bug fixes.
    I've been hard at working creating a progress bar. However, you might notice some dramatic changes to the existing codebase.
    
    Reasoning:
    Your old commit b903134 did not work for me at all. It was causing my Switch to not be found, I had to revert to the old code in order work the script to work again. I had to use the old code to implement my progress bar, therefore the ginormous code change.
    
    This commit did not return any bugs despite multiple NSP installs. However do not, some NSPs simply refuse to install and will crash. This seems to be an error on part of usb_install_pc.py original code and not tug.py.
    
    Following this commit I will also make a necessary commit to usb_install_pc.py that is necessary for this to work.

--- tug.py
+++ tug.py
@@ -27,69 +27,98 @@ SOFTWARE.
 
 import os
 import sys
+import threading
+file  = open("config.py", "w")
+file.write("cur_prog = 0\nend_prog = 0")
+file.close()
+import config
 from pathlib import Path
 from tkinter import messagebox
 from usb_install_pc import send_nsp_list, poll_commands
 import tkinter as tk
+root = tk.Tk()
+root.withdraw()
 
 """
 Exceptions for imports (PyUSB & PyQt5)
 from fourminute
 """
-root = tk.Tk()
-root.withdraw()
 try:
     import usb.core
     import usb.util
 except ImportError:
-    messagebox.showinfo("Error","PyUSB not found. Please install with 'pip3 install pyusb'\nIf you are on macOS, also install LibUSB with 'brew install libusb'.")
+    messagebox.showinfo("Error","PyUSB not found. Please install with 'pip3 install pyusb'\nIf you are on MacOS, also install LibUSB with 'brew install libusb'.")
     exit()
 try:
+    from PyQt5.QtCore import Qt, QThread
     from PyQt5 import QtWidgets, QtGui
-    from PyQt5.QtWidgets import QFileDialog, QListWidget, QMessageBox
+    from PyQt5.QtWidgets import QFileDialog, QListWidget, QMessageBox, QProgressBar, QApplication
 except ImportError:
     messagebox.showinfo("Error","PyQt5 not found. Please install with 'pip3 install pyqt5'.")
     exit()
 
+"""
+Seperate thread for installs.
+from fourminute
+"""
+class Install(QThread):
+    def __init__(self, nsp_dir, in_ep, out_ep):
+        super(Install, self).__init__()
+        self.nsp_dir = nsp_dir
+        self.in_ep = in_ep
+        self.out_ep = out_ep
+    def run(self):
+        try:
+            send_nsp_list(self.nsp_dir, self.out_ep)
+            poll_commands(self.nsp_dir, self.in_ep, self.out_ep)
+        except Exception as e:
+            print(str(e))
+            
 class TUG(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
+        self.init_ui()
+
+    """
+    Update progress bar.
+    from fourminute
+    """
+    def update_progress(self):
+        maxcount = self.list_header.count()
+        self.l_folder.setText(str(maxcount) + " NSPs selected.")
+        while True: 
+            try:
+                cur = config.cur_prog
+                end = config.end_prog
+                v = (cur / end) * 100
+                self.progressBar.setValue(v)
+                QApplication.processEvents()
+            except:
+                pass
+            
+    def init_ui(self):
         self.b_folder = QtWidgets.QPushButton("Select folder")
         self.b_header = QtWidgets.QPushButton("Send header")
         self.b_header.setEnabled(False)
         self.l_folder = QtWidgets.QLabel("No folder selected")
         self.list_header = QtWidgets.QListWidget()
         self.folder = ""
-
-        # Find Nintendo Switch
-        self.dev = usb.core.find(idVendor=0x057E, idProduct=0x3000)
-        if self.dev is None:
-            QMessageBox.warning(self, "Warning", "Nintendo Switch can't be found, try again.")
-            exit()
-        self.dev.reset()
-        self.dev.set_configuration()
-        cfg = self.dev.get_active_configuration()
-        is_out_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_OUT
-        is_in_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_IN
-        self.out_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_out_ep)
-        self.in_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_in_ep)
-        assert self.out_ep is not None
-        assert self.in_ep is not None
-
-        self.init_ui()
-
-    def init_ui(self):
-
+        self.progressBar = QProgressBar(self)
+        self.progressBar.setAlignment(Qt.AlignVCenter)
+        self.progressBar.setMaximum(100)
+        
         v_box = QtWidgets.QVBoxLayout()
         v_box.addWidget(self.b_folder)
         v_box.addWidget(self.list_header)
+        v_box.addWidget(self.progressBar)
 
         h_box = QtWidgets.QHBoxLayout()
         h_box.addWidget(self.l_folder)
         h_box.addWidget(self.b_header)
-
         v_box.addLayout(h_box)
+
+        
 
         self.setLayout(v_box)
         self.setWindowTitle('TUG')
@@ -98,9 +127,9 @@ class TUG(QtWidgets.QWidget):
         self.b_header.clicked.connect(self.send_header)
 
         self.setWindowIcon(QtGui.QIcon('icon.png'))
-
         self.show()
-
+        
+    
     def choose_folder(self):
         self.b_header.setEnabled(False)
         self.l_folder.setText("No folder selected")
@@ -117,15 +146,37 @@ class TUG(QtWidgets.QWidget):
             pass
         if len(self.folder) > 0:
             self.b_header.setEnabled(True)
-            self.folder = Path(self.folder)
-
+            
+                
     def send_header(self):
         try:
-            send_nsp_list(self.folder, self.out_ep)
-            poll_commands(self.folder, self.in_ep, self.out_ep)
+            nsp_dir = Path(self.folder)
         except:
-            QMessageBox.critical(self, "Critical", "Connection lost to Nintendo Switch, exiting.")
-            exit()
+            pass
+
+        dev = usb.core.find(idVendor=0x057E, idProduct=0x3000)
+        if dev is None:
+            QMessageBox.warning(self, "Warning", "Couldn't find Nintendo Switch")
+
+        try:
+            dev.reset()
+            dev.set_configuration()
+            cfg = dev.get_active_configuration()
+
+            is_out_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_OUT
+            is_in_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_IN
+            out_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_out_ep)
+            in_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=is_in_ep)
+
+            assert out_ep is not None
+            assert in_ep is not None
+            
+            self.install = Install(nsp_dir, in_ep, out_ep)
+            self.install.start()
+            self.b_header.setEnabled(False)
+            threading.Thread(target = self.update_progress).start()
+        except Exception as e:
+            print(str(e))
 
 app = QtWidgets.QApplication(sys.argv)
 ex = TUG()
